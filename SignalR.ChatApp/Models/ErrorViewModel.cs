0a0728820dedeff0d9628e4870ceba4e389c1a43
KubilayTopaktaş <KubilayTopakta@DESKTOP-PJ2UI5D>
2024-06-24 17:00:02 +0300

    Add project files.

---
+++ SignalR.ChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR.ChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
